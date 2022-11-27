1d9e5a7edff3e4da3561ce5488f3d657053ba1a2
won21yuk <won21yuk@gmail.com>
2022-11-27 23:56:25 +0900

    2022-11-27 main.tf used in terraform tutorial upload

---
+++ terrafrom-test/main.tf
@@ -0,0 +1,24 @@
+# main.tf
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  # 앞서 받은 서비스 계정키 퍼알(*.json)의 이름을 입력
+  credentials = file("terraform-practice-369907-098e7b100728.json")
+
+  # 자신의 프로젝트 아이디 입력
+  project = "terraform-practice-369907"
+  region  = "asia-northeast3"
+  zone    = "asia-northeast3-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
